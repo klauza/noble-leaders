48f62f79309fe83c8512cfd6a8eaefc6cf68bf9a
klauza <klauza.uk@gmail.com>
2019-07-31 20:36:24 +0100

    display links depending on logged in or not

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {logout} from '../../actions/loginActions';
 import history from '../../history';
 
-const Navbar = ({logout}) => {
+
+const Navbar = ({ login: {isAuthenticated}, logout }) => {
 
   const onLogout = () => {
     logout();
@@ -12,34 +13,58 @@ const Navbar = ({logout}) => {
     window.location.reload();
   }
 
+  const loggedInLinks = (
+    <Fragment>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/about'>About</Link>
+      </li>
+      <li>
+        <Link to='/profile'>Profile</Link>
+      </li>
+      <li>
+        <Link to='/leaderboard'>Leaderboard</Link>
+      </li>
+
+      <li>
+        <a href='#!' onClick={onLogout}>Logout</a>
+      </li>
+    </Fragment>
+  )
+
+  const notLoggedLinks = (
+    <Fragment>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/about'>About</Link>
+      </li>
+      <li>
+        <Link to='/leaderboard'>Leaderboard</Link>
+      </li>
+
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </Fragment>
+  )
+
   return (
     <div className="navigation-bar">
       <h3>Title</h3>
       <ul className="navigation-bar__ul">
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-        <li>
-          <Link to='/profile'>Profile</Link>
-        </li>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <a href='#!' onClick={onLogout}>Logout</a>
-        </li>
-        <li>
-          <Link to='/leaderboard'>Leaderboard</Link>
-        </li>
+        {isAuthenticated ? loggedInLinks : notLoggedLinks}
       </ul>
     </div>
   )
 }
-
-export default connect(null, {logout})(Navbar)
+const mapStateToProps = state => ({
+  login: state.login
+})
+export default connect(mapStateToProps, {logout})(Navbar)
