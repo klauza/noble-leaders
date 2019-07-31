89a564e3f96f62f76b67b0cc8b2af69460690c04
klauza <klauza.uk@gmail.com>
2019-07-31 15:31:43 +0100

    backend: db connection; auth routes

---
+++ backend/routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   POST api/users
+// @desc    register a user
+// @access  Public
+
+router.post('/', (req,res) => {
+  res.send('Register a user');
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// connect to database
+connectDB();
+
+// router endpoint
+app.get('/', (request, response) => response.send('<p>a</p>'));
+
+// define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
