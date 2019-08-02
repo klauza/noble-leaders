dcaad456177017ce5afb3b91f5265d8610cdaf0c
klauza <klauza.uk@gmail.com>
2019-08-02 02:33:24 +0100

    css: navbar

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {logout} from '../../actions/loginActions';
 import history from '../../history';
+import logo from '../../media/noble-leaders-logo.png';
 
 
 const Navbar = ({ login: {isAuthenticated}, logout }) => {
@@ -15,41 +16,44 @@ const Navbar = ({ login: {isAuthenticated}, logout }) => {
 
   const loggedInLinks = (
     <Fragment>
-      <li>
-        <Link to='/'>Home</Link>
+      
+      
+      <li className="nav-logo">
+        <Link to='/'><div className="logo-img"><img src={logo} alt=""/></div></Link>
       </li>
-      <li>
+      <li className="nav-about">
         <Link to='/about'>About</Link>
       </li>
-      <li>
-        <Link to='/profile'>Profile</Link>
-      </li>
-      <li>
+     
+      <li className="nav-leaderboard">
         <Link to='/leaderboard'>Leaderboard</Link>
       </li>
 
-      <li>
+      <li className="nav-logout">
         <a href='#!' onClick={onLogout}>Logout</a>
+      </li>
+      <li className="nav-profile">
+        <Link to='/profile'><i className="fa fa-user-circle"></i></Link>
       </li>
     </Fragment>
   )
 
   const notLoggedLinks = (
     <Fragment>
-      <li>
-        <Link to='/'>Home</Link>
+      <li className="nav-logo">
+        <Link to='/'><div className="logo-img"><img src={logo} alt=""/></div></Link>
       </li>
-      <li>
+      <li className="nav-about">
         <Link to='/about'>About</Link>
       </li>
-      <li>
+      <li className="nav-leaderboard">
         <Link to='/leaderboard'>Leaderboard</Link>
       </li>
 
-      <li>
+      <li className="nav-register">
         <Link to='/register'>Register</Link>
       </li>
-      <li>
+      <li className="nav-login">
         <Link to='/login'>Login</Link>
       </li>
     </Fragment>
@@ -57,8 +61,8 @@ const Navbar = ({ login: {isAuthenticated}, logout }) => {
 
   return (
     <div className="navigation-bar">
-      <h3>Title</h3>
-      <ul className="navigation-bar__ul">
+      
+      <ul className="navigation-bar__ul container">
         {isAuthenticated ? loggedInLinks : notLoggedLinks}
       </ul>
     </div>
