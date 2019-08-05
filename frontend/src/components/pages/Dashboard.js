9fe4000726a15879a97f1eb4d5c108b68d7c42f4
klauza <klauza.uk@gmail.com>
2019-08-05 16:22:24 +0100

    Dashboard: applied loader to each cover game image

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -1,23 +1,38 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import {loadUser} from '../../actions/loginActions';
 import { getUserGames } from '../../actions/gameActions';
 import {Link} from 'react-router-dom';
+import actorQuizCover from '../../media/games/actor-quiz-cover.jpg';
 
 const Dashboard = ({login: {isAuthenticated}, loadUser, getUserGames}) => {
+
+  const [img, setImg] = useState(true);
 
   useEffect(() => {
     if(localStorage.token) {
       loadUser();
       getUserGames(null);
     }
+
+    function loadImageAsync(image){
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.addEventListener('load', event => resolve(img));
+        img.addEventListener('erorr', reason => reject(new Error('error')));
+        img.src = image
+      })
+    }
+    loadImageAsync(actorQuizCover)
+      .then(() => setImg(false))
+      .catch(reason => console.log(reason));
     //eslint-disable-next-line
   }, []);
 
   return (
     <div className="container dashboard">
       <div className="grid">
-        <div className="dashboard-actor-game-cover"><Link to='/actor-game' className="grid-link"></Link></div>
+        <div className={`${img ? "image-placeholder" : "dashboard-actor-game-cover"}`}><Link to='/actor-game' className="grid-link"></Link></div>
         <div className="dashboard-snake-cover"><Link to='/snake' className="grid-link"></Link></div>
         <div></div>
         <div></div>
