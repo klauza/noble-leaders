0bd74d14d8ad5d87df3ec2768414dbf2cb0e8250
klauza <klauza.uk@gmail.com>
2019-08-05 16:20:00 +0100

    Add Loader component

---
+++ frontend/src/components/layout/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import loader from '../../media/loader.gif';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img src={loader} alt="loader" />
+    </div>
+  )
+}
+
+export default Loader
