994e51f5d05defb5726ed28259d7aa79fadd1c3e
klauza <klauza.uk@gmail.com>
2019-08-06 15:45:08 +0100

    added Promise.all as cover images are loading

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -4,6 +4,7 @@ import {loadUser} from '../../actions/loginActions';
 import { getUserGames } from '../../actions/gameActions';
 import {Link} from 'react-router-dom';
 import actorQuizCover from '../../media/games/actor-quiz-cover.jpg';
+import snakeCover from '../../media/games/snake-cover.png';
 import LoaderPlaceholder from '../layout/Loader';
 
 const Dashboard = ({login: {isAuthenticated}, loadUser, getUserGames}) => {
@@ -16,15 +17,24 @@ const Dashboard = ({login: {isAuthenticated}, loadUser, getUserGames}) => {
       getUserGames(null);
     }
 
-    function loadImageAsync(image){
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.addEventListener('load', event => resolve(img));
-        img.addEventListener('erorr', reason => reject(new Error('error')));
-        img.src = image
+    function loadImageAsync(image1, image2){
+      return Promise.all([
+      new Promise((resolve, reject) => {
+        const img1 = new Image();
+        img1.addEventListener('load', event => resolve(img1));
+        img1.addEventListener('erorr', reason => reject(new Error('error')));
+        img1.src = image1
+      }),
+      new Promise((resolve, reject) => {
+        const img2 = new Image();
+        img2.addEventListener('load', event => resolve(img2));
+        img2.addEventListener('erorr', reason => reject(new Error('error')));
+        img2.src = image2
       })
+    
+      ])
     }
-    loadImageAsync(actorQuizCover)
+    loadImageAsync(actorQuizCover, snakeCover)
       .then(() => setImg(false))
       .catch(reason => console.log(reason));
     //eslint-disable-next-line
@@ -34,8 +44,8 @@ const Dashboard = ({login: {isAuthenticated}, loadUser, getUserGames}) => {
     <div className="container dashboard">
       <div className="grid">
         <div className={`grid-item ${!img && "dashboard-actor-game-cover"}`}> {img ? <LoaderPlaceholder /> : <Link to='/actor-game' className="grid-link"></Link> } </div>
-        {/* <div className={`${img ? "image-placeholder" : "dashboard-actor-game-cover"}`}><Link to='/actor-game' className="grid-link"></Link></div> */}
-        <div className="dashboard-snake-cover"><Link to='/snake' className="grid-link"></Link></div>
+        <div className={`grid-item ${!img && "dashboard-snake-cover"}`}> {img ? <LoaderPlaceholder /> : <Link to='/snake' className="grid-link"></Link> } </div>
+
         <div></div>
         <div></div>
         <div></div>
