55489c030e0c46277afa1086f2f75ce17109d62a
klauza <klauza.uk@gmail.com>
2019-08-08 00:42:16 +0100

    Initialize Snake game

--- frontend/src/components/games/snake/Snake.js
+++ frontend/src/components/games/snake/Snake.js
@@ -1,26 +1,252 @@
-import React, {useEffect} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import { connect } from 'react-redux';
-import { loadUser } from '../../../actions/loginActions';
-import { getUserGames } from '../../../actions/gameActions';
+import { setAlert } from '../../../actions/alertActions';
+import { loadUser, userUpdate } from '../../../actions/loginActions';
+import { getUserGames, updateGameScore } from '../../../actions/gameActions';
+import { EAST, NORTH, SOUTH, WEST, initialState, enqueue, next} from './main.js';
 
 
-const Snake = ({getUserGames, loadUser}) => {
+
+const Snake = ({login: {isAuthenticated, user}, game: { current, games }, setAlert, userUpdate, getUserGames, updateGameScore, loadUser}) => {
+  const [canvas, setCanvas] = useState(null);
+  const [apples, setApples] = useState(0);
+  const [block, setBlock] = useState(false);
+  const [localScore, setLocalScore] = useState(null);
+
 
   useEffect(() => {
-    async function snakeInit(){
-      await loadUser();
-      await getUserGames("snake");
+    if(localStorage.token) {
+      async function snakeInit(){
+        await loadUser();
+        await getUserGames("snake");
+      }
+      snakeInit();
+      setCanvas(document.getElementById('canvas'));
+    } else {
+      setCanvas(document.getElementById('canvas'));
     }
-    snakeInit();
-   
     //eslint-disable-next-line
-  }, [])
+  }, []);
+
+  let ctx = null;
+  
+
+  
+  
+  function startTheSnake() {
+
+  if(canvas){
+    ctx = canvas.getContext('2d');
+  
+    // Mutable state
+    let state = initialState()
+
+    // Position helpers
+    const x = column => Math.round(column * canvas.width / state.cols)
+    const y = row => Math.round(row * canvas.height / state.rows)
+
+
+    // Game loop draw
+    const draw = () => {
+      
+      // console.log(state.obstacle);
+      
+      // clear
+      ctx.fillStyle = '#232323'
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+      // draw snake
+      ctx.fillStyle = 'rgb(0,200,50)'
+      state.snake.map(p => ctx.fillRect(x(p.x), y(p.y), x(1), y(1)))
+
+      // draw apples
+      ctx.fillStyle = 'rgb(255,50,0)'
+      ctx.fillRect(x(state.apple.x), y(state.apple.y), x(1), y(1))
+
+      // draw obstacles
+      ctx.fillStyle = '#fff';
+      state.obstacle.map( p => ctx.fillRect(x(p.x), y(p.y), x(1), y(1)))
+
+      
+      setApples(state.score);
+      
+      // add crash
+      if (state.snake.length === 0) {
+        console.log('crash');
+        
+        if(isAuthenticated){
+
+          
+          let roundScore = parseInt(state.score, 10); 
+          
+
+          async function updateActorGameScore(){
+            // await getUserGames("snake");
+            let entryScore = await document.querySelector('.tutajKurwa').textContent;
+            entryScore = parseInt(entryScore, 10);
+            console.log('entry: ',entryScore, 'thisRound: ',roundScore);
+            
+            if(roundScore > entryScore){
+              if(entryScore === 0){
+                setAlert("Congratulations! Now see your score in your Profile!", 'danger');
+                async function sumScore(){
+                  let userTotalScore = await user.highscore;
+                  console.log('totalscore: ',userTotalScore);
+                  console.log('roundscore: ',roundScore);
+
+                  userTotalScore = await userTotalScore + roundScore;   // just increment total entryScore
+
+                  // update user's highscore
+                  const updUserHighscore = await {
+                    _id: user._id,
+                    highscore: userTotalScore,
+                    date: new Date()
+                  }
+                  await loadUser();
+                  await userUpdate(updUserHighscore);
+
+                }
+                sumScore();
+
+                async function sumGameScore(){
+                  // update current game
+                  const updScore = await {
+                    _id: current._id,
+                    score: roundScore,
+                    date: new Date()
+                  }
+                  await updateGameScore(updScore);
+                  
+                }
+                await sumGameScore();
+                await loadUser();
+                await getUserGames("snake");
+
+              } else {
+                // update score if higher than previous
+                setAlert("You have beaten your score, nice job!", 'danger');
+                async function sumScore(){
+                  let userTotalScore = await user.highscore;
+                  userTotalScore = await userTotalScore - entryScore;
+                  userTotalScore = await userTotalScore + roundScore;   // just increment total entryScore
+                  console.log('totalscore: ', userTotalScore);
+              
+                  // update user's highscore
+                  const updUserHighscore = await {
+                    _id: user._id,
+                    highscore: userTotalScore,
+                    date: new Date()
+                  }
+                  await loadUser();
+                  await userUpdate(updUserHighscore);
+
+                
+                }
+                sumScore();
+
+
+                async function sumGameScore(){
+                  // update current game
+                  const updScore = await {
+                    _id: current._id,
+                    score: roundScore,
+                    date: new Date()
+                  }
+                  await updateGameScore(updScore);
+                  
+                }
+                await sumGameScore();
+                await loadUser();
+                await getUserGames("snake");
+              }
+            } else {
+              //console.log('nothing to update, your score was lower');
+              setAlert("Unfortunately you didn't beat your score", 'danger');
+              return
+            }
+          }
+          updateActorGameScore().then(() => {
+            console.log('refresh here');
+            window.location.reload(true);
+          });
+          
+
+
+       }else {
+        // If not logged in
+        //console.log('please log in to update the score');
+        setAlert('please log in to update the score', 'danger');
+      }
+  
+
+       
+
+
+        
+        
+
+        ctx.fillStyle = 'rgb(255,0,0)'
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+        state.score = 0;  // reset score, save score
+      }
+      
+  
+    }
+
+    // Game loop update
+    const step = t1 => t2 => {
+      if (t2 - t1 > 100) {
+        state = next(state)
+        draw()
+        window.requestAnimationFrame(step(t2))
+      } else {
+        window.requestAnimationFrame(step(t1))
+      }
+    }
+
+    // Key events
+    window.addEventListener('keydown', e => {
+      switch (e.key) {
+        case 'w': case 'h': case 'ArrowUp':    state = enqueue(state, NORTH); break
+        case 'a': case 'j': case 'ArrowLeft':  state = enqueue(state, WEST);  break
+        case 's': case 'k': case 'ArrowDown':  state = enqueue(state, SOUTH); break
+        case 'd': case 'l': case 'ArrowRight': state = enqueue(state, EAST);  break
+      }
+    })
+
+    // Main
+    draw();
+    window.requestAnimationFrame(step(0))
+    
+  }
+}
+
+
+const startTheSnakeNow = () => {
+
+    if(block !== true){
+      startTheSnake();
+    }
+    setBlock(true);
+}
+
+
+
 
   return (
-    <div>
-      Welcome to snake game
-    </div>
+    <Fragment>
+      <div className="snake-the-game-container">
+        <canvas id="canvas" width="700" height="500" onClick={startTheSnakeNow}></canvas>
+        {apples}
+      </div>
+      <span className="tutajKurwa">{current && current.score}</span>
+    </Fragment>
   )
 }
 
-export default connect(null, {getUserGames, loadUser})(Snake);
+const mapStateToProps = state => ({
+  login: state.login,
+  game: state.game
+})
+export default connect(mapStateToProps, {getUserGames, loadUser, updateGameScore, setAlert, userUpdate})(Snake);
---
+++ frontend/src/components/games/snake/main.js
@@ -0,0 +1,119 @@
+// BASE
+const adjust    = n => f => xs => mapi(x => i => i == n ? f(x) : x)(xs)
+const dropFirst = xs => xs.slice(1)
+const dropLast  = xs => xs.slice(0, xs.length - 1)
+const id        = x => x
+const k         = x => y => x
+const map       = f => xs => xs.map(f)
+const mapi      = f => xs => xs.map((x, i) => f(x)(i))
+const merge     = o1 => o2 => Object.assign({}, o1, o2)
+const mod       = x => y => ((y % x) + x) % x // http://bit.ly/2oF4mQ7
+const objOf     = k => v => ({ [k]: v })
+const pipe      = (...fns) => x => [...fns].reduce((acc, f) => f(acc), x)
+const range     = n => m => Array.apply(null, Array(m - n)).map((_, i) => n + i)
+const rep       = c => n => map(k(c))(range(0)(n))
+const rnd       = min => max => Math.floor(Math.random() * max) + min
+const spec      = o => x => Object.keys(o)  // it takes keys out of object [x is avalue]
+  .map(k => objOf(k)(o[k](x)))  // maps over the keyes
+  .reduce((acc, o) => Object.assign(acc, o))
+
+// Constants
+const NORTH = { x: 0, y:-1 }
+const SOUTH = { x: 0, y: 1 }
+const EAST  = { x: 1, y: 0 }
+const WEST  = { x:-1, y: 0 }
+
+// Point operations
+const pointEq = p1 => p2 => p1.x == p2.x && p1.y == p2.y
+
+
+// Booleans
+const willEat   = state => pointEq(nextHead(state))(state.apple)
+const willSelfCrash = state => state.snake.find(pointEq(nextHead(state)))
+
+const willObstacleCrash = state => state.obstacle.find(pointEq(nextHead(state))) // if head will be on obstacle position ==> dead
+
+const validMove = move => state =>
+  state.moves[0].x + move.x != 0 || state.moves[0].y + move.y != 0
+
+
+// Next values based on state
+const nextMoves = state => state.moves.length > 1 // if the moves list is greater than 0
+  ? state.moves.slice(1) // return first = use first 
+  : state.moves
+
+const nextApple = state => willEat(state)   // if snake will eat the apple
+ ? rndApplePos(state)    // we need to create the next one
+ : state.apple      // if he won't eat, leave on current position
+
+const nextHead  = state => state.snake.length == 0
+  ? {x: 2, y: 2} // random position? -> rndPos(state)
+  : {
+    x: mod(state.cols)(state.snake[0].x + state.moves[0].x),
+    y: mod(state.rows)(state.snake[0].y + state.moves[0].y)
+  }
+const nextSnake = state => willSelfCrash(state)
+  ? [] 
+  : (willObstacleCrash(state)) ? []
+  : (willEat(state))
+    ? [nextHead(state)].concat(state.snake)
+    : [nextHead(state)].concat(dropLast(state.snake))
+
+const nextScore = state => willEat(state) ? state.score = state.score + 1 : state.score
+
+
+// Randomness and check if apple rendered on any obstacle
+const rndApplePos = state => {
+  let newApple
+  function createApple(){
+    newApple = ({
+      x: rnd(0)(state.cols - 1),
+      y: rnd(0)(state.rows - 1)
+    })
+  }
+  createApple();
+  console.log(newApple);
+  state.obstacle.forEach((obs) => {
+    if(JSON.stringify(obs) === JSON.stringify(newApple)){
+      createApple();
+    } 
+  })
+  
+  return newApple
+}
+
+// random
+const rndPos = table => ({
+  x: rnd(0)(table.cols - 1),
+  y: rnd(0)(table.rows - 1)
+})
+
+
+// Initial state
+const initialState = () => ({
+  cols:  20,
+  rows:  14,
+  moves: [EAST],
+  snake: [{x: 2, y: 2}],
+  apple: { x: 7, y: 2 },
+  score: 0,
+  obstacle: [{x: 5, y: 8}, {x: 6, y: 8}, {x: 7, y: 8}, {x: 11, y: 11}, {x: 12, y: 11}, {x: 11, y: 12}, {x: 12, y: 12}, {x: 0, y: 7}, {x: 0, y: 8}, {x: 0, y: 9}, {x: 0, y: 10}]
+})
+
+// next is a function that takes a state and produces new object
+const next = state => ({
+  rows: state.rows,
+  cols: state.cols,
+  moves: nextMoves(state),
+  snake: nextSnake(state),
+  apple: nextApple(state),
+  score: nextScore(state),
+  obstacle: state.obstacle
+})
+
+
+const enqueue = (state, move) => validMove(move)(state)
+  ? merge(state)({ moves: state.moves.concat([move]) })
+  : state
+
+module.exports = { EAST, NORTH, SOUTH, WEST, initialState, enqueue, next }
