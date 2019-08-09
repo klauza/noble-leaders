e97c64417cae184a919fe9124048c289ac867223
klauza <klauza.uk@gmail.com>
2019-08-09 21:33:19 +0100

    css: About

--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -12,12 +12,48 @@ const About = ({login: {isAuthenticated}, loadUser}) => {
   }, []);
   
   return (
-    <div className="container about">
-      <h2 className="mt-5">About project</h2>
-      <p>version 1.0</p>
-      <p>Author: github-link</p>
-      <a href="https://www.linkedin.com/in/michal-klauza-b22318186/"><i className="fa fa-linkedin-square"></i></a>
-      <a href="https://github.com/klauza/noble-leaders"><i className="fa fa-github-square"></i></a>
+    <div className="about">
+      <div className="about__main">
+
+        <div className="about__main--project">
+          <h1>Hey, amazing stranger, thanks for coming to see my work</h1>
+          <p>NOBLE-LEADERS is a minimalistic game-center project with subtle design and mechanics.</p>
+          <p>You can create an account with safe password hashing. So please do so - without it, you won't be able to see a lot of things.</p>
+          <p>There are plenty of games available to play - they are not great, but give the score points as a return.</p>
+          <p>After few hours of playing... ( ͡º ͜ʖ ͡º) you can check the leaderboard if you are a chief among the noble ones.</p>
+        </div>
+
+        <div className="about__main--expression">
+          <p>To be honest, as you might have already noticed, there is nothing special or amazing in this project, but you must know that I've put a lot of effort and time into hand-craft every piece of this page. Nevertheless in the same time I've had tons of fun (drinking million cups of coffee) while solving programming riddles and learning new stuff. At the end, I'm happy to show you my coding journey.</p>
+          <p>I'm hoping for constructive criticism from your side in comment section.</p>
+          <p>Thank you!</p>
+          <p>I wish you a great day!</p>
+        </div>
+      
+        <div className="about__main--links">
+          <div className="git-container">
+            <a href="https://github.com/klauza/noble-leaders">
+              <span>Github</span> 
+              <i className="fa fa-github-square"></i>
+            </a>
+          </div>
+
+          <div className="linked-container">
+            <a href="https://www.linkedin.com/in/michal-klauza-b22318186/">
+              <span>LinkedIn</span>
+              <i className="fa fa-linkedin-square"></i>
+            </a>
+          </div>
+        </div>
+     
+        <div className="about__main--footer">
+          <p>version 1.0</p> 
+          <p>This project is being constantly developed and maintained since idea creation on 31/07/2019.</p>
+          <p>Author: Michal Klauza</p>
+          <span>Image attributions links on github</span>
+        </div>
+
+      </div>
     </div>
   )
 }
