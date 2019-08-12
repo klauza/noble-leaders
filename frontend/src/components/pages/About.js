1ccbb2b9f9e88faf801ac6d73bac66af5e5d26be
klauza <klauza.uk@gmail.com>
2019-08-12 13:03:15 +0100

    about page text/style change

--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -17,30 +17,28 @@ const About = ({login: {isAuthenticated}, loadUser}) => {
 
         <div className="about__main--project">
           <h1>Hey, amazing stranger, thanks for coming to see my work</h1>
-          <p>NOBLE-LEADERS is a minimalistic game-center project with subtle design and mechanics.</p>
-          <p>You can create an account with safe password hashing - please do so, without it, you won't be able to see a lot of stuff.</p>
-          <p>There are plenty of games available to play - they are not great, but give the score points as a return.</p>
-          <p>After few hours of playing... ( ͡º ͜ʖ ͡º) you can check the leaderboard if you are a chief among the noble ones.</p>
+          <p><strong>NOBLE-LEADERS</strong> is a minimalistic <strong>fullstack</strong> project, also mobile friendly, with subtle design and mechanics.</p>
+          <p>How to start? You can create an account with safe password hashing - please do so, without it, you won't be able to see a lot.</p>
+          <p>There are plenty of games available to play - they give score points letting you climb on top of leaderboard.</p>
         </div>
 
         <div className="about__main--expression">
-          <p>To be honest, as you might have already noticed, there is nothing special or amazing in this project, but you must know that I've put a lot of effort and time into hand-craft every piece of this page. Nevertheless in the same time I've had tons of fun (drinking million cups of coffee) while solving programming riddles and learning new stuff. At the end, I'm happy to show you my coding journey.</p>
-          <p>I'm hoping for constructive criticism from your side in comment section.</p>
+          <p>This project is being constantly developed and maintained since idea creation on 31/07/2019. You must know that I've put a lot of effort and time into hand-craft every piece of this page. Nevertheless in the same time I've had tons of fun (drinking million cups of coffee) while solving programming riddles and learning new stuff. At the end, I'm happy to show you my coding journey.</p>
+          <p>I'm hoping for constructive criticism from your side.</p>
           <p>Thank you!</p>
           <p>I wish you a great day!</p>
+          <p style={{"fontSize": "0.8rem", "color": "grey"}}>ps. I'm looking for a nice job{` --`}<i className="fa fa-thumbs-up"></i></p>
         </div>
       
         <div className="about__main--links">
           <div className="git-container">
             <a href="https://github.com/klauza/noble-leaders">
-              <span>Github</span> 
               <i className="fa fa-github-square"></i>
             </a>
           </div>
 
           <div className="linked-container">
             <a href="https://www.linkedin.com/in/michal-klauza-b22318186/">
-              <span>LinkedIn</span>
               <i className="fa fa-linkedin-square"></i>
             </a>
           </div>
@@ -48,9 +46,8 @@ const About = ({login: {isAuthenticated}, loadUser}) => {
      
         <div className="about__main--footer">
           <p>version 1.0</p> 
-          <p>This project is being constantly developed and maintained since idea creation on 31/07/2019.</p>
-          <p>Author: Michal Klauza</p>
-          <span>Image attributions links - see on github</span>
+          <p>Copyright: Michal Klauza</p>
+          <p>Image attributions links - see on github</p>
         </div>
 
       </div>
