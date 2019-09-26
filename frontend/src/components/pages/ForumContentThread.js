a659a06fef7301b19eaa35d06d7a35b357993d57
klauza <klauza.uk@gmail.com>
2019-09-26 15:44:49 +0100

    Forum - reading thread's content

--- frontend/src/components/pages/ForumContentThread.js
+++ frontend/src/components/pages/ForumContentThread.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import forumData from './ForumContentThreadData'; // data
+import Loader from '../layout/Loader';
 
-const ForumThread = () => {
-  return (
-    <div className="forum-content-thread">
-      A certain thread...
-    </div>
-  )
+const ForumThread = (props) => {
+  const [article, setArticle] = useState(null);
+  const articleName = props.match.params.thread;
+
+  useEffect(()=>{ 
+    // fetch certain article
+    setArticle( forumData.filter((article, id) => article.title === articleName)[0] )
+    
+  }, [])
+
+  console.log(article);
+  // console.log(articleName);
+
+  if(article !== null){
+    return (
+      <div className="forum-content-thread">
+        {article.content}
+      </div>
+    )
+    
+  } else {
+    return(
+      <Loader />
+    )
+  }
+
 }
 
 export default ForumThread
---
+++ frontend/src/components/pages/ForumContentThreadData.js
@@ -0,0 +1,21 @@
+const forumData = [
+  {
+    title: "about-website",
+    content: "lorem ipsum dolor sit 1"
+  },
+  {
+    title: "leaderboard-tournament",
+    content: "lorem ipsum dolor sit 2"
+  },
+  {
+    title: "snake-discussion",
+    content: "lorem ipsum dolor sit 3"
+  },
+  {
+    title: "about-me",
+    content: "lorem ipsum dolor sit 4"
+  }
+]
+
+
+export default forumData
