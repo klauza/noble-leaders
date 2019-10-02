5d10d2d613640f870aa34b19a4736f3a47ba7de2
klauza <klauza.uk@gmail.com>
2019-10-02 14:12:58 +0100

    revealCards game - cards loader: UX update

--- frontend/src/components/games/reveal-cards/RevealCards.js
+++ frontend/src/components/games/reveal-cards/RevealCards.js
@@ -16,6 +16,7 @@ import { lang1, lang2, lang3, lang4, lang5, lang6 } from '../../../media/images'
 const RevealCards = ({login: {loading, isAuthenticated, user}, game: { current, gLoading }, setAlert, loadUser, getUserGames, updateGameScore, userUpdate}) => {
   
   const [cards, setCards] = useState(null);
+  const [pageReady, setPageReady] = useState(false);
   
   const [entryAttempts, setEntryAttempts] = useState(null);
   const [theEntryScore, setTheEntryScore] = useState(null);
@@ -49,17 +50,34 @@ const RevealCards = ({login: {loading, isAuthenticated, user}, game: { current, 
       var theCards = chosenCards;
       var newMap = new Array(12); // create empty array 
       let index = 0;
-    
-  
+
+
+      // Load all images on page load
+      const checkImage = (path) =>
+      new Promise(resolve => {
+          const img = new Image();
+          img.onload = () => resolve({path, status: 'ok'});
+          img.onerror = () => resolve({path, status: 'error'});
+          console.log(img);
+
+          img.src = path;
+      }); 
+      const loadImg = (images) => Promise.all(images.map(img => checkImage(img)));
+
       do{
         let randNum = Math.floor(Math.random()*theCards.length); // 0 - 11, decreasing
         newMap[index] = theCards[randNum];
         theCards.splice(randNum, 1);
-  
+
+        
+
         index++;
       } while(theCards.length > 0);
   
       setCards(newMap);
+      loadImg(newMap); 
+      setPageReady(true);
+      
     }
 
     
@@ -100,7 +118,7 @@ const RevealCards = ({login: {loading, isAuthenticated, user}, game: { current, 
         </main>
         
       </div>
-      { (cards === null) ? <Loader /> : <CardsEngine setEntryAttempts={setEntryAttempts} setTheRoundScore={setTheRoundScore} isAuthenticated={isAuthenticated} cards={cards}/> }
+      { (cards === null || pageReady === false) ? <Loader /> : <CardsEngine setEntryAttempts={setEntryAttempts} setTheRoundScore={setTheRoundScore} isAuthenticated={isAuthenticated} cards={cards}/> }
       {current && entryAttempts !== null && theEntryScore !== null && theRoundScore !== null ? <UpdateThisGame user={user} current={current} theAttempts={entryAttempts} theGame={"reveal-cards"} setEntryAttempts={setEntryAttempts} setTheEntryScore={setTheEntryScore} theEntryScore={theEntryScore} theRoundScore={theRoundScore} /> : null }
       
      
