f1230e54cf521615aa2038a501d473056b145da9
Michal Klauza <klauza.uk@gmail.com>
2020-05-22 11:03:15 +0100

    change default ports that app runs on

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ if(process.env.NODE_ENV === 'production'){
 
   app.use('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
 }
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5100;
 
 app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
